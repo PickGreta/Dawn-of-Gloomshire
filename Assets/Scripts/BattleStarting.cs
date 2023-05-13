cee49b523505f0deb0bb76f38cabd39eafea3fba
PickGreta <pickermann.greta@gmail.com>
2023-05-13 17:28:38 +0200

    Battle System

---
+++ Assets/Scripts/BattleStarting.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleStarting : MonoBehaviour
+{
+    public GameObject battleSceneObject;
+
+    private void OnMouseDown()
+    {
+        battleSceneObject.SetActive(true);
+
+        GameObject battleSystemObject = GameObject.Find("BattleSystem");
+
+        BattleSystem battleScene = battleSystemObject.GetComponent<BattleSystem>();
+        battleScene.MoveCameraToNewPosition();
+    }
+
+}
